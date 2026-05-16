be90ea7dab528520cf6fbae6fb032a5a3d65b15c
lxl2000927 <3030679951@qq.com>
2026-05-17 00:18:42 +0800

    build: 升级到 1.1.4 版本

--- scripts/build_release.ps1
+++ scripts/build_release.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 Set-Location (Split-Path -Parent $PSScriptRoot)
 
-$Version = "1.1.3.0"
+$Version = "1.1.4.0"
 
 python -m pip install --upgrade pip
 python -m pip install --upgrade pyinstaller
--- src/version.py
+++ src/version.py
@@ -1,5 +1,5 @@
 APP_NAME = "PDF Split"
-APP_VERSION = "1.1.3"
-APP_FILE_VERSION = "1.1.3.0"
+APP_VERSION = "1.1.4"
+APP_FILE_VERSION = "1.1.4.0"
 GITHUB_REPO = "LXL2000927/file-toolbox"
 GITHUB_RELEASES_PAGE = f"https://github.com/{GITHUB_REPO}/releases"
